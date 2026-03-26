90fdec0855140f32191a8da6771ab25d78e41bbe
Katsuhide Tsuruta <katsuhide.tsuruta@gmail.com>
2026-03-26 21:18:14 +0900

    fix: server sends game.state to all players after scenario generation, remove client reconnect

--- ios/Madaminu/Sources/Network/WSMessageAdapter.swift
+++ ios/Madaminu/Sources/Network/WSMessageAdapter.swift
@@ -10,8 +10,6 @@ struct WSMessageAdapter {
             if store.screen != .generating { store.screen = .generating }
         case "game.ready":
             store.screen = .intro
-            // Reconnect to get fresh game.state with scenario data
-            store.reconnectWebSocket()
         case "images.ready":
             store.reconnectWebSocket()
         case "phase.started":
